0f98a0e231bef4aae4a0a379377700ae9877e6ac
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2023-01-23 22:31:04 +0500

    Добавьте файлы проекта.

---
+++ R_3_2_3/Main.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI_3_2_3
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<FamilyInstance> fInstances = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_StructuralColumns)
+                .WhereElementIsNotElementType()
+                .Cast<FamilyInstance>()
+                .ToList();
+
+            TaskDialog.Show("Количество колонн", fInstances.Count.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
